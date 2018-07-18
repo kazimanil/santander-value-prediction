d7ba7b7eaf9df67d41646d084c480a6efb18224e
Kazım Anıl Eren <33596334+kazimanil@users.noreply.github.com>
2018-07-18 12:26:41 +0300

    Fixed Train and Test datasets.

--- Santander_DataPrep.R
+++ Santander_DataPrep.R
@@ -7,19 +7,19 @@ library("xgboost")
 
 # Exclude Variables which Test & Train distribute differently.
 summary =
-	data.table(colname    = colnames(train[, 3:ncol(train)]),
-						 train_sd   = as.data.table(sapply(train[, 3:ncol(train)], sd, na.rm = TRUE))$V1,
-						 train_mean = as.data.table(sapply(train[, 3:ncol(train)], mean, na.rm = TRUE))$V1,
-						 train_rows = nrow(train),
-						 test_sd    = as.data.table(sapply(test[, 2:ncol(test)], sd, na.rm = TRUE))$V1,
-						 test_mean  = as.data.table(sapply(test[, 2:ncol(test)], mean, na.rm = TRUE))$V1,
-						 test_rows  = nrow(test))
+    data.table( colname    = colnames(train[, 3:ncol(train)]),
+                train_sd   = as.data.table(sapply(train[, 3:ncol(train)], sd, na.rm = TRUE))$V1,
+                train_mean = as.data.table(sapply(train[, 3:ncol(train)], mean, na.rm = TRUE))$V1,
+                train_rows = nrow(train),
+                test_sd    = as.data.table(sapply(test[, 2:ncol(test)], sd, na.rm = TRUE))$V1,
+                test_mean  = as.data.table(sapply(test[, 2:ncol(test)], mean, na.rm = TRUE))$V1,
+                test_rows  = nrow(test))
 summary[, t_value := (train_mean - test_mean) / sqrt((train_sd^2/train_rows) + (test_sd^2 /test_rows))]
 summary[, t_pos := qt(p = 0.99, df = nrow(test) + nrow(train) - 2)]
 summary[, t_neg := -qt(p = 0.99, df = nrow(test) + nrow(train) - 2)]
 selected_cols = summary[t_value >= t_neg & t_value <= t_pos]$colname
-train = train[, selected_cols, with = FALSE]
-test  = test[, selected_cols, with = FALSE]
+train = train[, append("ID", append("target", selected_cols)), with = FALSE]
+test  = test[, append("ID",selected_cols), with = FALSE]
 rm(summary, selected_cols)
 
 # Next step should be to use PCA to combine variables into new variables.
