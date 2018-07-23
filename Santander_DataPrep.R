2dcc4cb7e01dc83d60cdd47a866e2be136e0c945
Kazım Anıl Eren <anil.eren@digitallency.com>
2018-07-23 18:10:59 +0300

    Slow Daily Progress for Understanding PCA in R

--- Santander_DataPrep.R
+++ Santander_DataPrep.R
@@ -3,23 +3,62 @@ train = fread("train.csv")
 test  = fread("test.csv")
 sample = fread("sample_submission.csv")
 
-library("xgboost")
+# Libraries ----
+library("stats")      # Factor Analysis
+#library("psych")     # Factor Analysis -- stats::princomp has a better interface.
+library("xgboost")    # Gradient Boosting
 
-# Exclude Variables which Test & Train distribute differently.
+# Data Manipulation ----
+# Since there are lots of variables I will have to make a selection. Thereby, I will first decide if variables distribute differently in test and train sets assuming they distribute normally. I will use student's t-distribution and %99 confidence interval for this test. Then I will filter out variables which fail the test.
+
 summary =
-    data.table( colname    = colnames(train[, 3:ncol(train)]),
-                train_sd   = as.data.table(sapply(train[, 3:ncol(train)], sd, na.rm = TRUE))$V1,
-                train_mean = as.data.table(sapply(train[, 3:ncol(train)], mean, na.rm = TRUE))$V1,
-                train_rows = nrow(train),
-                test_sd    = as.data.table(sapply(test[, 2:ncol(test)], sd, na.rm = TRUE))$V1,
-                test_mean  = as.data.table(sapply(test[, 2:ncol(test)], mean, na.rm = TRUE))$V1,
-                test_rows  = nrow(test))
+  data.table( colname    = colnames(train[, 3:ncol(train)]),
+              train_sd   = as.data.table(sapply(train[, 3:ncol(train)], sd, na.rm = TRUE))$V1,
+              train_mean = as.data.table(sapply(train[, 3:ncol(train)], mean, na.rm = TRUE))$V1,
+              train_rows = nrow(train),
+              test_sd    = as.data.table(sapply(test[, 2:ncol(test)], sd, na.rm = TRUE))$V1,
+              test_mean  = as.data.table(sapply(test[, 2:ncol(test)], mean, na.rm = TRUE))$V1,
+              test_rows  = nrow(test))
 summary[, t_value := (train_mean - test_mean) / sqrt((train_sd^2/train_rows) + (test_sd^2 /test_rows))]
 summary[, t_pos := qt(p = 0.99, df = nrow(test) + nrow(train) - 2)]
 summary[, t_neg := -qt(p = 0.99, df = nrow(test) + nrow(train) - 2)]
 selected_cols = summary[t_value >= t_neg & t_value <= t_pos]$colname
 train = train[, append("ID", append("target", selected_cols)), with = FALSE]
 test  = test[, append("ID",selected_cols), with = FALSE]
+
+# More than half of the variables cannot pass. After deciding which columns to keep I will merge it with the original data. However, some columns have type mismatch. So I will turn all of them into numeric variables ( rather then integer in some cases).
+
+train = cbind(train[, 1:2],
+              sapply(train[, 3:ncol(train)], as.numeric))
+test  = cbind(test[, 1],
+              sapply(test[, 2:ncol(test)], as.numeric))
 rm(summary, selected_cols)
 
 # Next step should be to use PCA to combine variables into new variables.
+dt_pca = rbind(train[, 3:ncol(train)], test[, 2:ncol(test)])
+n_cols = ncol(dt_pca)  # Number of columns
+dt_cor = cor(dt_pca)   # Correlation Matrix
+dt_eig = eigen(dt_cor) # EigenValues & EigenVectors
+dt_eiv = dt_eig$values # EigenValues
+n_facs = length(dt_eiv[dt_eiv > 1]) # Amount of Factors to be derived based on "EigenValue > 1" criterion. Let's depict this with a graph as well.
+
+plot(dt_eiv,
+     main="Scree Plot",
+     ylab="Eigenvalues",
+     xlab="Component number",
+     type='b')
+abline(h=1, lty=2)
+
+rm(dt_cor, dt_eig, dt_eiv); gc()
+pca_results = prcomp(x = dt_pca, retx = TRUE, scale = TRUE, tol = 0.5)
+# pca_results2 = princomp(x = dt_pca)
+# pca_results3 = psych::principal(r = dt_cor, nfactors = n_facs, rotate = "varimax", scores = TRUE, n.obs = nrow(dt_pca))
+
+# Now let's construct new data.tables:
+new_train  = cbind(train[, 1:2], 
+                   pca_results$x[1:nrow(train), 1:n_facs])
+test_start = nrow(train)+1
+test_end   = nrow(test)+nrow(train)
+new_test   = cbind(test[, 1], 
+                   pca_results$x[test_start:test_end, 1:n_facs])
+rm(test_start, test_end)
