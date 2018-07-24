8685b7d25a46118a505da9f3ab20236da239db48
Kazım Anıl Eren <anil.eren@digitallency.com>
2018-07-24 16:28:27 +0300

    completed PCA.

--- Santander_DataPrep.R
+++ Santander_DataPrep.R
@@ -7,6 +7,7 @@ sample = fread("sample_submission.csv")
 library("stats")      # Factor Analysis
 #library("psych")     # Factor Analysis -- stats::princomp has a better interface.
 library("xgboost")    # Gradient Boosting
+library("MlBayesOpt") # Optimisation for Gradient Boosting.
 
 # Data Manipulation ----
 # Since there are lots of variables I will have to make a selection. Thereby, I will first decide if variables distribute differently in test and train sets assuming they distribute normally. I will use student's t-distribution and %99 confidence interval for this test. Then I will filter out variables which fail the test.
@@ -49,16 +50,45 @@ plot(dt_eiv,
      type='b')
 abline(h=1, lty=2)
 
-rm(dt_cor, dt_eig, dt_eiv); gc()
-pca_results = prcomp(x = dt_pca, retx = TRUE, scale = TRUE, tol = 0.5)
+pca_results = psych::principal(r = dt_cor, nfactors = n_facs, rotate = "varimax", scores = TRUE, n.obs = nrow(dt_pca))
+pca_weights = as.matrix(pca_results$loadings)
+class(pca_weights) = "matrix"
+pca_weights = as.data.table(pca_weights)
+pca_weights = sapply(pca_weights, FUN = function(x) ifelse(x < 0.3 & x > -0.3, 0, x), simplify = TRUE)
+# pca_results  = prcomp(x = dt_pca, retx = TRUE, scale = TRUE, tol = 0.5)
 # pca_results2 = princomp(x = dt_pca)
-# pca_results3 = psych::principal(r = dt_cor, nfactors = n_facs, rotate = "varimax", scores = TRUE, n.obs = nrow(dt_pca))
 
 # Now let's construct new data.tables:
+new_dt     = as.matrix(dt_pca) %*% pca_weights
+colnames   = as.character(seq(1, n_facs, 1))
+colnames   = paste0("factor", colnames)
+new_dt     = as.data.table(new_dt)
+new_dt     = `colnames<-`(new_dt, colnames)
+
+rm(dt_cor, dt_eig, dt_eiv, pca_results, pca_weights); gc()
 new_train  = cbind(train[, 1:2], 
-                   pca_results$x[1:nrow(train), 1:n_facs])
+                   new_dt[1:nrow(train), 1:n_facs])
 test_start = nrow(train)+1
 test_end   = nrow(test)+nrow(train)
 new_test   = cbind(test[, 1], 
-                   pca_results$x[test_start:test_end, 1:n_facs])
-rm(test_start, test_end)
+                   new_dt[test_start:test_end, 1:n_facs])
+rm(test_start, test_end, n_cols, n_facs, colnames, dt_pca, test, train, new_dt)
+save(new_test, new_train, file = "PCA.RData")
+
+#xGBoost -- The model parameters are obtained with bayesian optimization package (https://www.kaggle.com/kailex/santander-eda-features)
+p <- list(objective = "reg:linear",
+          booster = "gbtree",
+          eval_metric = "rmse",
+          nthread = 8,
+          eta = 0.0075,
+          max_depth = 30,
+          min_child_weight = 52,
+          gamma = 0.09690536,
+          subsample = 0.95,
+          colsample_bytree = 0.1,
+          colsample_bylevel = 0.1,
+          alpha = 0,
+          lambda = 100,
+          nrounds = 10000)
+
+m_xgb <- xgb.train(p, dtrain, p$nrounds, list(val = dval), print_every_n = 100, early_stopping_rounds = 300)
